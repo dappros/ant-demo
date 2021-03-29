3e4efe958dfccb48a4be86650fa5a5030c9cb2e1
Miltosh <68873959+Miltosh@users.noreply.github.com>
2021-03-29 17:41:43 +0300

    changed email-js user

--- src/pages/New Negotiation/components/emailSendler.jsx
+++ src/pages/New Negotiation/components/emailSendler.jsx
@@ -4,27 +4,30 @@ import emailjs from 'emailjs-com';
 export const sendEmailToBob = (e) => {
     e = e || window.event;
 
-    e.preventDefault();    //This is important, i'm not sure why, but the email won't send without it
+    e.preventDefault(); //This is important, i'm not sure why, but the email won't send without it
 
-    emailjs.sendForm('gmail', 'toBob', e.target, 'user_ERKzPIkK5byCFbAKgFKlO')
-        .then((result) => {
-            //window.location.reload()  //This is if you still want the page to reload (since e.preventDefault() cancelled that behavior) 
-        }, (error) => {
+    emailjs.sendForm('gmail', 'toBob', e.target, 'user_tGCSMnM3NeKnFUPua11U2').then(
+        (result) => {
+            //window.location.reload()  //This is if you still want the page to reload (since e.preventDefault() cancelled that behavior)
+        },
+        (error) => {
             console.log(error.text);
-        });
-
-}
+        },
+    );
+};
 
 // Send email, when Bob submited form
 export const sendEmailToAlice = (e) => {
     e = e || window.event;
 
-    e.preventDefault();    //This is important, i'm not sure why, but the email won't send without it
+    e.preventDefault(); //This is important, i'm not sure why, but the email won't send without it
 
-    emailjs.sendForm('gmail', 'toAlice', e.target, 'user_ERKzPIkK5byCFbAKgFKlO')
-        .then((result) => {
-            //window.location.reload()  //This is if you still want the page to reload (since e.preventDefault() cancelled that behavior) 
-        }, (error) => {
+    emailjs.sendForm('gmail', 'toAlice', e.target, 'user_tGCSMnM3NeKnFUPua11U2').then(
+        (result) => {
+            //window.location.reload()  //This is if you still want the page to reload (since e.preventDefault() cancelled that behavior)
+        },
+        (error) => {
             console.log(error.text);
-        });
-}
+        },
+    );
+};
